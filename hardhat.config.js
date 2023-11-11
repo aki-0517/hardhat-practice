16b0d80ca90d3bd5e70bf70fa3ee9b6568d98509
aki-0517 <akki.pp.777@gmail.com>
2023-11-11 23:58:09 +0900

    add deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+const ROPSTEN_RPC_URL = process.env.ROPSTEN_RPC_URL;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    ropsten: {
+      url: `${ROPSTEN_RPC_URL}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
